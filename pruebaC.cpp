310a88fa917fbc33bfa62f75c80022f93cee071a
Daniel Forero <df.forero10@uniandes.edu.co>
2017-01-24 16:53:25 -0500

    test

--- pruebaC.cpp
+++ pruebaC.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <stdio.h>
 #include <math.h>
+#include <fstream>
+
 using namespace std;
  
 int main () {
@@ -15,10 +17,21 @@ srand( (unsigned)time( NULL ) );
 for( i = 0; i < 100; i++ ) {
 double u=((double)rand()/(double)RAND_MAX);
 double x=log(1-u)/(-3);
-nums[i]=x
+nums[i]=10*(x+1);
+//cout << x << '\n';
 }
-FILE *f = fopen("numlist.csv", "w");
-fprintf(f, "%f\n",nums);
-fclose(f);
+ofstream myfile ("numlist.csv");
+
+if (myfile.is_open())
+  {
+
+
+    for(int count = 0; count < 99; count ++){
+        myfile << nums[count] << "\n" ;
+    }
+    myfile.close();
+  }
+  else cout << "Unable to open file";
+
 return 0;
 }
